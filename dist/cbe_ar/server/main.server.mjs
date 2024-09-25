d97fe9f1ff2fa6464e1e0cf52804ecc20e30a2d9
adduup <adwait.uprety21@gmail.com>
2024-09-25 14:34:45 -0500

    3c

---
+++ dist/cbe_ar/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-E2QMZCST.mjs";import"./chunk-B26VWFYV.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
 export class AppComponent {
-  title = 'cbe_ar';
+  artist = {
+    email: '',
+    bio: '',
+    name: '',
+    
+  };
+
+  constructor(private http: HttpClient) {}
+
+  onSubmitArtist() {
+    this.http.post('http://localhost:8080/api/artist/submit', this.artist)
+      .subscribe(response => {
+        console.log('Artist details submitted:', response);
+        alert('Form submitted successfully');
+      }, error => {
+        console.error('Error submitting form:', error);
+        alert('Form submitted successfully');
+      });
+  }
 }
