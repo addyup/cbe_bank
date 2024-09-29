610726cdd97f03170f7df3139177b0e35f193120
adduup <adwait.uprety21@gmail.com>
2024-09-28 19:24:46 -0500

    added routing and button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,56 +1,21 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, ViewChild } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
-import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
+
+import { Component } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, MatSidenavModule,
-    MatListModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatButtonModule,
+  imports: [RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
 export class AppComponent {
-  artist = {
-    email: '',
-    bio: '',
-    name: '',
-    
-  };
 
-  constructor(private http: HttpClient) {}
 
-  onSubmitArtist() {
-    this.http.post('https://calculated.onrender.com/api/artist/submit', this.artist)
-      .subscribe(response => {
-        console.log('Artist details submitted:', response);
-        alert('Form submitted successfully');
-      }, error => {
-        console.error('Error submitting form:', error);
-        alert('Form submitted successfully');
-      });
+  constructor( public router: Router) {}
+
+  navigateToSignup() {
+    this.router.navigate(['/signup']);
   }
-
-  // @ViewChild('sidenav') sidenav!: MatSidenav;
-
-  // toggleSidenav() {
-  //   this.sidenav.toggle();
-  // }
-
-  opened = false; // Controls sidenav state
-
-  toggleSidenav() {
-    this.opened = !this.opened; // Toggle state
-  }
-  
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppComponent }, // Default route
+    { path: 'signup', component: SignUpComponent } // Signup route
+  
+];
